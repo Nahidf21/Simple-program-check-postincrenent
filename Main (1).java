a627d4bc6a854196cf1a783119dddef76254a708
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2022-04-11 09:50:47 -0500

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int c=5;
+    System.out.printf("c before postincrement: %d%n", c);
+    System.out.printf("   postincrenenting c: %d%n", c++);
+    System.out.printf("c after postincrement: %d%n", c);
+    System.out.println();
+    c=5;
+    System.out.printf("c before postincrement: %d%n", c);
+    System.out.printf("   postincrenenting c: %d%n", ++c);
+    System.out.printf("c after postincrement: %d%n", c);
+    
+  }
+}
